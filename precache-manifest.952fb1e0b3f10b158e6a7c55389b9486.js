e81dacb93f6c7cae10d2375e5fc9815ebb783c79
tlhamo <tsekilhamo@gmail.com>
2020-08-27 16:04:52 -0500

    Updates

---
+++ precache-manifest.952fb1e0b3f10b158e6a7c55389b9486.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4912a430f7c99a07cfabe1ba4348da",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "774834a24501218e4f6a",
+    "url": "/recipeapp/static/css/main.4c8d22b0.chunk.css"
+  },
+  {
+    "revision": "778a7a0e260e8b73c8fa",
+    "url": "/recipeapp/static/js/2.6711340c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipeapp/static/js/2.6711340c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774834a24501218e4f6a",
+    "url": "/recipeapp/static/js/main.ebec2361.chunk.js"
+  },
+  {
+    "revision": "2c6823dfb356579ecc04",
+    "url": "/recipeapp/static/js/runtime-main.83b6e7d9.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/recipeapp/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/recipeapp/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
